6718b6d164f283870e11d72bfe1f7e8ba69dc4ad
aissam83 <aissam.ourhou@live.com>
2019-10-03 16:47:18 +0200

    first commit

---
+++ src/main/java/com/f2i/Calculator.java
@@ -0,0 +1,11 @@
+package com.f2i;
+
+public class Calculator {
+
+	public int add(int a, int b) {
+	return a +b;
+}
+	public int mutiply(int a, int b) {
+		return a * b;
+	}
+}
---
+++ src/main/java/com/f2i/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.f2i;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("!! Hello World !! I2A ");
+		System.out.println("!! Tous Va Bien !! ");
+	}
+
+}
